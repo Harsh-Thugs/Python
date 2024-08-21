aefb6b7897bbf916f3af4a74bda799351ca4fb99
Harsh Chavan <harshchavan5490@gmail.com>
2024-08-20 21:23:53 -0700

    Create write_a_program_to_check_input_number_is_prime_number_or_not

---
+++ write_a_program_to_check_input_number_is_prime_number_or_not.py
@@ -0,0 +1,14 @@
+
+n=int(input("enter"))
+if(n==2):
+    {
+        print(" prime number")
+    }
+elif(n%2==0 or n==1):
+        {
+            print("Entered number is not prime")
+        }
+else:
+            {
+                print("Entered number is prime")
+            }
