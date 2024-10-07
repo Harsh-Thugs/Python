dbd5770f36eef832b16044fa97cd7668d1adb782
Harsh Chavan <harshchavan5490@gmail.com>
2024-10-07 09:38:35 +0530

    Update object_orentation_programing

--- object_orentation_programing.py
+++ object_orentation_programing.py
@@ -7,4 +7,17 @@ class Dog: #Class
 my_dog = Dog("Sheru",2) #Object(intsance of dog)
 print("Name: ",my_dog.name)
 print("Age: ",my_dog.age)
-print(my_dog.bark())
+print(my_dog.bark()) 
+
+------------------------------------------------------------------------------------------------------------------
+
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def greet(self):
+      return f"Hello, my name  is {self.name} and I am {self.age} years old."
+person1 = person("Kamlesh",25)
+print("Name: ", person1.name)
+print("age: ", person1.age)         
+print(person1.greet())
