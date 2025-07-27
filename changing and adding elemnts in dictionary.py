7ef6e7b857a6886015254d5b666ff895eef1216b
Harsh Chavan <harshchavan5490@gmail.com>
2025-07-27 11:30:03 +0530

    Update changing and adding elemnts in dictionary.py

--- changing and adding elemnts in dictionary.py
+++ changing and adding elemnts in dictionary.py
@@ -1,5 +1,5 @@
 #changing and adding elemnts in dictionary
-dict={1:"Harsh",2:"Abhishek",3:"Shreyash",4:"Krishnanu"}
+dict={1:"Harsh",2:"Abhishek",3:"Karthik",4:"Krishnanu"}
 print(dict)
 dict[4]="MOMMY"
 print(dict)
